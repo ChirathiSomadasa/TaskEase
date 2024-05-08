a4dc7012619743b66b3118dd885a16cf8f7379d2
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-05-08 23:14:25 +0530

    add navigation to the onboarding1

--- app/src/main/java/com/chirathi/taskease/MainActivity.kt
+++ app/src/main/java/com/chirathi/taskease/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.chirathi.taskease
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Onboarding1Activity::class.java)
+            startActivity(intent)
+        }, 1000)
     }
 }
