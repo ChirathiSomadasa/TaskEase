812589b5be89684bbdde8eb9a4d6f041a8376ecd
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-05-11 07:07:33 +0530

    Implement ViewModel

--- app/src/main/java/com/chirathi/taskease/MainActivity.kt
+++ app/src/main/java/com/chirathi/taskease/MainActivity.kt
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.ViewModelProvider
+import com.chirathi.taskease.database.TaskDatabase
+import com.chirathi.taskease.repository.TaskRepository
+import com.chirathi.taskease.viewmodel.TaskViewModel
+import com.chirathi.taskease.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-
+    private fun setupViewModel(){
+        val taskRepository = TaskRepository(TaskDatabase(this))
+        val viewModelProviderFactory = TaskViewModelFactory(application,taskRepository)
+        taskViewModel = ViewModelProvider(this,viewModelProviderFactory)[TaskViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/chirathi/taskease/viewmodel/TaskViewModel.kt
@@ -0,0 +1,28 @@
+package com.chirathi.taskease.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.chirathi.taskease.model.Task
+import com.chirathi.taskease.repository.TaskRepository
+import kotlinx.coroutines.launch
+
+class TaskViewModel(app: Application, private  val taskRepository: TaskRepository):AndroidViewModel(app) {
+
+
+    fun addTask(task: Task) = viewModelScope.launch {
+        taskRepository.insertTask(task)
+    }
+
+    fun deleteTask(task: Task) = viewModelScope.launch {
+        taskRepository.deleteTask(task)
+    }
+
+    fun updateTask(task: Task) = viewModelScope.launch {
+        taskRepository.insertTask(task)
+    }
+
+    fun getAllNotes() = taskRepository.getAllTasks()
+
+    fun searchTask(query: String?) = taskRepository.searchTask(query)
+}
